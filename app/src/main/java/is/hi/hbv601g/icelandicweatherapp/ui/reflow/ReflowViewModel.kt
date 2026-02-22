e51f94438eb70a4a25f3f552d44b8e8c08f792a8
masdernii <amsig59@gmail.com>
2026-02-22 16:58:11 +0000

    initial commit

---
+++ app/src/main/java/is/hi/hbv601g/icelandicweatherapp/ui/reflow/ReflowViewModel.kt
@@ -0,0 +1,13 @@
+package `is`.hi.hbv601g.icelandicweatherapp.ui.reflow
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class ReflowViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is reflow Fragment"
+    }
+    val text: LiveData<String> = _text
+}
