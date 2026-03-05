8f6a10aca4c6d640b142cef6cbabe22b719bceaf
masdernii <amsig59@gmail.com>
2026-03-05 14:27:12 +0000

    ui fixes

--- app/src/main/java/is/hi/hbv601g/icelandicweatherapp/MainActivity.kt
+++ app/src/main/java/is/hi/hbv601g/icelandicweatherapp/MainActivity.kt
@@ -42,7 +42,7 @@ class MainActivity : AppCompatActivity() {
         binding.navView?.let {
             appBarConfiguration = AppBarConfiguration(
                 setOf(
-                    R.id.nav_transform, R.id.nav_reflow, R.id.nav_slideshow, R.id.nav_settings
+                    R.id.nav_transform, R.id.nav_locations, R.id.nav_slideshow, R.id.nav_settings
                 ),
                 binding.drawerLayout
             )
@@ -53,7 +53,7 @@ class MainActivity : AppCompatActivity() {
         binding.appBarMain.contentMain.bottomNavView?.let {
             appBarConfiguration = AppBarConfiguration(
                 setOf(
-                    R.id.nav_transform, R.id.nav_reflow, R.id.nav_slideshow
+                    R.id.nav_transform, R.id.nav_locations, R.id.nav_slideshow
                 )
             )
             setupActionBarWithNavController(navController, appBarConfiguration)
