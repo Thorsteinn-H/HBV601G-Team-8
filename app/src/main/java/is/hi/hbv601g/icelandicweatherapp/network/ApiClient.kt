b7391b3bc63bf812577e047c1f8b1e67a26a7a49
thors <thorsteinnhilmir@gmail.com>
2026-03-22 20:31:04 +0000

    add files that somehow didnt go through

---
+++ app/src/main/java/is/hi/hbv601g/icelandicweatherapp/network/ApiClient.kt
@@ -0,0 +1,42 @@
+package `is`.hi.hbv601g.icelandicweatherapp.network
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+/**
+ * Defines the API base URL
+ * Configures JSON parsing (Gson)
+ * Exposes the VedurApi interface
+ */
+object ApiClient {
+
+    /**
+     * Base URL = https://api.vedur.is/cap/v1/
+     *
+     */
+    private const val BASE_URL = "https://api.vedur.is/cap/v1/"
+
+    /**
+     * lazy means its not created until api is accesssed the first time
+     *  - saves memory and startup
+     */
+    val api: VedurApi by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            //converter for JSON into Kotlin data classes
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(VedurApi::class.java)
+    }
+    private const val NORTHERN_LIGHTS_BASE_URL = "https://services.swpc.noaa.gov/"
+
+    private val northernLightsRetrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(NORTHERN_LIGHTS_BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+    val northernLightsApi: NorthernLightsApi by lazy {
+        northernLightsRetrofit.create(NorthernLightsApi::class.java)
+    }
+}
